e3cf0e593349e53438a35702a04e0b64b3caca90
SEJO <sead.bander@gmail.com>
2022-04-26 11:39:52 +0200

    webApp commit

---
+++ out/artifacts/webApp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: SEJO
+  Date: 4/22/2022
+  Time: 15:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Welcome</title>
+  </head>
+  <body>
+  <h1>Ovo je dobrodošlica</h1>
+  </body>
+</html>
---
+++ src/main/java/WelcomeServlet.java
@@ -0,0 +1,36 @@
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.annotation.WebServlet;
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//import java.time.LocalDateTime;
+//@WebServlet(name = "welcome", urlPatterns = {"/welcome"})
+//
+//public class WelcomeServlet extends HttpServlet {
+//
+//    private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+//        resp.setContentType("text/html");
+//        try (PrintWriter out = resp.getWriter()) {
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Welcome servlet</title>");
+//            out.println("<body>");
+//            out.println("Pozdrav iz WELCOME SERVLETA. Uspio si. Tacno vrijeme je: " + LocalDateTime.now());
+//            out.println("</body>");
+//            out.println("</head>");
+//            out.println("</html>");
+//        }
+//    }
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        processRequest(req, resp);
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//
+//    }
+//}
